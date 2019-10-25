2466f36e1b6326fc52df1a1f6def1394378bba60
l8090100 <l8090100@gmail.com>
2019-10-25 17:29:50 +0800

    add arduino controller  program

---
+++ Wonder/20170602controller/_20170602controller.ino
@@ -0,0 +1,32 @@
+
+/*END_STOP_B = 22
+MANUAL_SWITCH_TGB_ON = 23;
+MANUAL_SWITCH_TGB_OFF = 24;
+MANUAL_BAR_A_UP = 25;
+MANUAL_BAR_A_DW = 26;
+MANUAL_BAR_B_UP = 27;
+MANUAL_BAR_B_DW = 28;
+MANUAL_BAR_C_UP = 29;
+MANUAL_BAR_C_DW = 30;
+MANUAL_BAR_D_UP = 31;
+MANUAL_BAR_D_DW = 32;
+CUE_1_B = 33;
+CUE_2_B = 34;
+CUE_3_B = 35;
+CUE_4_B = 36;
+CUE_5_B = 37;
+CUE_6_B = 38;
+STOP_B = 39;
+PLAY_B = 40;
+PAUSE_B = 41;
+*/
+
+boolean trigger[20] = {22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41}; 
+void setup() {
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
